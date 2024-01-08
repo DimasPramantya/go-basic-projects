ce78a407dd9adde297a081478fc12bbf72e4d921
DimasPramantya <igkdimas@gmail.com>
2024-01-09 01:53:52 +0700

    investment formula fixed

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,12 +1,20 @@
 package main
+
 import "fmt"
 
-func main(){
-	var investmentAmmount = 5000.0;
-	var returnRate = 5.5;
-	var years = 10;
+import "math"
 
-	var futureValue = investmentAmmount * returnRate * float64(years);
+func main(){
+	var investmentAmmount = 5000;
+	var returnRate = 5.5;
+	
+	//shorthand to declare and assign new variable
+	years := 10;
+
+	futureValue := float64(investmentAmmount) * math.Pow((1 + returnRate / 100), float64(years));
+
+	//round 3 decimal places
+	futureValue = math.Round(futureValue * 1000) / 1000;
 
 	fmt.Print("Future ammount is $", futureValue);
 }
